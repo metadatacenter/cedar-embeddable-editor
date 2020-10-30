7dcdb9fa90923daf92a9b5b491baf788b1f9f84b
Attila L. Egyedi <attila.egyedi@gmail.com>
2020-10-30 12:18:14 -0700

    Remove json-cycle import

--- src/app/modules/shared/pipe/secure-json.pipe.ts
+++ src/app/modules/shared/pipe/secure-json.pipe.ts
@@ -1,12 +1,11 @@
 import {Pipe, PipeTransform} from '@angular/core';
-import {decycle} from 'json-cycle';
 
 @Pipe({name: 'secureJson'})
 export class SecureJsonPipe implements PipeTransform {
 
   transform(value: any): string {
     console.log('SecureJsonPipe.transform');
-    return JSON.stringify(decycle(value));
+    return JSON.stringify(value);
   }
 
 }
