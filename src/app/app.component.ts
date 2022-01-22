096f7ab3e20d5d92b10028d768a59727b0d31b11
mdorf <mdorf@stanford.edu>
2022-01-21 16:01:41 -0800

    material theme compatibility enhancements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,7 @@ export class AppComponent {
 
     sampleTemplateLocationPrefix: 'https://component.metadatacenter.orgx/cedar-embeddable-editor-sample-templates/',
     // sampleTemplateLocationPrefix: 'https://component.staging.metadatacenter.org/cedar-embeddable-editor-sample-templates/',
-    loadSampleTemplateName: '01',
+    loadSampleTemplateName: '12',
     showSampleTemplateLinks: true,
     expandedSampleTemplateLinks: true,
 
