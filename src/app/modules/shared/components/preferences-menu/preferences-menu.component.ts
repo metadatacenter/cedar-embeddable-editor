2dc67418bcf31eba63a17eaf1df98398ac6cc250
Mete Akdogan <muakdogan@gmail.com>
2023-05-19 09:35:41 -0700

    WIP:adds pref menu

---
+++ src/app/modules/shared/components/preferences-menu/preferences-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'preferences-menu',
+  templateUrl: './preferences-menu.component.html',
+  styleUrls: ['./preferences-menu.component.scss']
+})
+export class PreferencesMenu {
+
+}
