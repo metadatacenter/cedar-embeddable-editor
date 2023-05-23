e58e9a6440b095f4da48bb8a350b014c3b5fe5f9
Mete Ugur Akdogan <muakdogan@gmail.com>
2023-05-23 16:54:54 -0700

    reverts config to original

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import {Component} from '@angular/core';
 export class AppComponent {
 
   ceeConfig = {
-    showTemplateUpload: false,
+    showTemplateUpload: true,
     templateUploadBaseUrl: 'http://localhost:8000',
     // templateUploadBaseUrl: 'https://api-php.cee.metadatacenter.orgx',
     templateUploadEndpoint: '/upload.php',
@@ -16,22 +16,22 @@ export class AppComponent {
     templateUploadParamName: '3520cf061bba4919a8ea4b74a07af01b',
     templateDownloadParamName: '9ff482bacac84c499655ab58efdf590a',
 
-    showDataSaver: false,
+    showDataSaver: true,
     dataSaverEndpointUrl: 'http://localhost:8000/datasave.php',
 
     sampleTemplateLocationPrefix: 'https://component.metadatacenter.orgx/cedar-embeddable-editor-sample-templates/',
     // sampleTemplateLocationPrefix: 'https://component.staging.metadatacenter.org/cedar-embeddable-editor-sample-templates/',
     loadSampleTemplateName: '45',
-    showSampleTemplateLinks: false,
+    showSampleTemplateLinks: true,
     expandedSampleTemplateLinks: true,
-    showTemplateRenderingRepresentation: false,
+    showTemplateRenderingRepresentation: true,
 
     // terminologyProxyUrl: 'https://api-php.cee.metadatacenter.orgx/index.php',
     // terminologyProxyUrl: 'http://localhost:8000/index.php',
     terminologyProxyUrl: 'https://terminology.metadatacenter.org/bioportal/integrated-search',
-    showInstanceDataCore: false,
-    expandedInstanceDataCore: true,
-    showMultiInstanceInfo: false,
+    showInstanceDataCore: true,
+    expandedInstanceDataCore: false,
+    showMultiInstanceInfo: true,
     expandedMultiInstanceInfo: false,
     collapseStaticComponents: true,
 
