36ccbdabf2c340c5bef75521738cdcded9d9ae4b
Attila L. Egyedi <egyedia@stanford.edu>
2023-11-02 11:33:28 -0700

    Change version to 1.0.1 Add changelog entries, remove duplicate stylesheet inclusion

--- src/app/app.component.dev.ts
+++ src/app/app.component.dev.ts
@@ -9,7 +9,7 @@ export class AppComponentDev implements OnInit {
 
   ceeConfig = {
     sampleTemplateLocationPrefix: 'http://localhost:4240/cedar-embeddable-editor-sample-templates/',
-    loadSampleTemplateName: '65',
+    loadSampleTemplateName: '21',
     showSampleTemplateLinks: true,
     expandedSampleTemplateLinks: true,
     showTemplateRenderingRepresentation: true,
