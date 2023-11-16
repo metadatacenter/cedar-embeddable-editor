98fecbc27b21dcc0fe53394ada9dfab6391e75ef
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2023-11-16 15:58:09 -0800

    Fix "NullPointerExceptions"s Fixes: https://github.com/metadatacenter/cedar-embeddable-editor/issues/79

--- src/app/app.component.dev.ts
+++ src/app/app.component.dev.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponentDev implements OnInit {
   ceeConfig = {
     sampleTemplateLocationPrefix: 'http://localhost:4240/cedar-embeddable-editor-sample-templates/',
-    loadSampleTemplateName: '02',
+    loadSampleTemplateName: '66',
     showSampleTemplateLinks: true,
     expandedSampleTemplateLinks: false,
     showTemplateRenderingRepresentation: true,
