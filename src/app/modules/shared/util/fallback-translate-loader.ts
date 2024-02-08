3d941d08cbe3af37f461f3e7d5821eefe220576b
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2024-02-08 11:07:09 -0800

    Fix default language loading if not specified. Better language related logging.

--- src/app/modules/shared/util/fallback-translate-loader.ts
+++ src/app/modules/shared/util/fallback-translate-loader.ts
@@ -22,33 +22,47 @@ export class FallbackTranslateLoader implements TranslateLoader {
     const languageMapPathPrefix = this.globalSettingsContextService.languageMapPathPrefix;
 
     if (languageMapPathPrefix != null) {
-      this.messageHandlerService.trace('Loading language map from config path: "' + languageMapPathPrefix + '"');
-      this.messageHandlerService.trace('Loading language map: "' + lang + '"');
+      this.messageHandlerService.traceGroup(
+        'language',
+        'Loading language map from config path: "' + languageMapPathPrefix + '"',
+      );
+      this.messageHandlerService.traceGroup('language', 'Loading language map: "' + lang + '"');
       const httpLoader = new TranslateHttpLoader(this.http, languageMapPathPrefix);
       return httpLoader.getTranslation(lang).pipe(
         tap({
           next: () => {
-            this.messageHandlerService.trace('External language map loaded.');
+            this.messageHandlerService.traceGroup('language', 'External language map loaded.');
           },
         }),
         catchError(() => {
-          this.messageHandlerService.trace('External language map not found, using built-in version.');
+          this.messageHandlerService.traceGroup('language', 'External language map not found, using built-in version.');
           return this.getBuiltInVersion(lang);
         }),
       );
     } else {
-      this.messageHandlerService.trace('"languageMapPathPrefix" not set, using built-in language map.');
+      this.messageHandlerService.traceGroup(
+        'language',
+        '"languageMapPathPrefix" not set, using built-in language map.',
+      );
       return this.getBuiltInVersion(lang);
     }
   }
 
   getBuiltInVersion(lang: string): Observable<any> {
     if (Object.hasOwn(this.fallback, lang)) {
-      this.messageHandlerService.trace('Using built-in language map for "' + lang + '"');
+      this.messageHandlerService.traceGroup('language', 'Using built-in language map for "' + lang + '"');
       return of(this.fallback[lang]);
     } else {
-      this.messageHandlerService.trace('No built-in language map for "' + lang + '"');
-      return of(null);
+      const defaultLanguage = GlobalSettingsContextService.DEFAULT_LANGUAGE;
+      this.messageHandlerService.traceGroup(
+        'language',
+        'No built-in language map for "' +
+          lang +
+          '", using default built-in language map for "' +
+          defaultLanguage +
+          '"',
+      );
+      return of(this.fallback[defaultLanguage]);
     }
   }
 }
