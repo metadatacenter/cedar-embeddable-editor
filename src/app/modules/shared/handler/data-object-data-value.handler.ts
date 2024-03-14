ac909bc0c55f06f9dfad74a8b32e5e2a83ac7d8e
Mete Ugur Akdogan <muakdogan@gmail.com>
2024-03-13 20:17:27 -0700

    WIP: debugging

--- src/app/modules/shared/handler/data-object-data-value.handler.ts
+++ src/app/modules/shared/handler/data-object-data-value.handler.ts
@@ -14,6 +14,7 @@ import { CedarModel } from '../models/cedar-model.model';
 import { DataObjectUtil } from '../util/data-object-util';
 import { InputType } from '../models/input-type.model';
 import { MessageHandlerService } from '../service/message-handler.service';
+import { at } from 'lodash-es';
 
 export class DataObjectDataValueHandler {
   private messageHandlerService: MessageHandlerService;
@@ -174,6 +175,10 @@ export class DataObjectDataValueHandler {
     multiInstanceObjectService: MultiInstanceObjectHandler,
     path: string[],
   ): object {
+    console.log('Data object', dataObject);
+    console.log('Component', component);
+    console.log('multi', multiInstanceObjectService);
+    console.log('Path', path);
     const obj = {};
     const firstPath = path[0];
     const remainingPath = path.slice(1);
@@ -195,6 +200,7 @@ export class DataObjectDataValueHandler {
         multiInstanceObjectService.getMultiInstanceInfoForComponent(multiElement);
       const currentIndex = multiInstanceInfo.currentIndex;
       childComponent = multiElement.getChildByName(firstPath);
+      console.log('DataObject', dataObject);
       dataSubObject = dataObject[currentIndex][firstPath];
       parentDataSubObject = dataObject[currentIndex];
     }
@@ -405,7 +411,7 @@ export class DataObjectDataValueHandler {
       valueObject[JsonSchema.atId] = atId;
       valueObject[JsonSchema.rdfsLabel] = prefLabel;
     }
-
+    console.log('Datacontext', dataContext);
     this.setDataPathValueRecursively(
       dataContext.instanceExtractData,
       null,
