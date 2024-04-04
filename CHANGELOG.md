d7ed58439980df5576aaffd4429090e3d199027a
Mete Ugur Akdogan <muakdogan@gmail.com>
2024-04-04 09:19:22 -0700

    updates for release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,15 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.3.2]
+
+### Added
+- Link to BioPortal controlled terms
+- Link to ORCID and ROR ids
+
+### Fixed
+- Hiding state of nested empty elements
+
 ## [1.3.1]
 
 ### Added
--- src/app/modules/shared/components/cedar-embeddable-metadata-editor/cedar-embeddable-metadata-editor.component.ts
+++ src/app/modules/shared/components/cedar-embeddable-metadata-editor/cedar-embeddable-metadata-editor.component.ts
@@ -15,7 +15,7 @@ import { MessageHandlerService } from '../../service/message-handler.service';
   encapsulation: ViewEncapsulation.None,
 })
 export class CedarEmbeddableMetadataEditorComponent implements OnInit {
-  private static INNER_VERSION = '2024-03-20 08:50:00';
+  private static INNER_VERSION = '2024-04-04 09:18:00';
 
   private static SHOW_TEMPLATE_RENDERING = 'showTemplateRenderingRepresentation';
   private static SHOW_MULTI_INSTANCE = 'showMultiInstanceInfo';
