7e625ed2f0ae5c93a0ad3014fad210e23901b839
Mete Ugur Akdogan <muakdogan@gmail.com>
2025-03-06 10:48:40 -0800

    adds ROR and ORCID details

--- src/app/modules/shared/service/orcid-field-data.service.ts
+++ src/app/modules/shared/service/orcid-field-data.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, timer } from 'rxjs';
-import { RorSearchResponse } from '../models/rest/ror-search/ror-search-response';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { MessageHandlerService } from './message-handler.service';
 import { map, switchMap } from 'rxjs/operators';
 import { OrcidSearchResponse } from '../models/rest/orcid-search/orcid-search-response';
 import { OrcidSearchResponseItem } from '../models/rest/orcid-search/orcid-search-response-item';
@@ -14,13 +12,10 @@ export class OrcidFieldDataService {
   private orcidSearchUrl = 'https://bridge.metadatacenter.orgx/ext-auth/orcid/search-by-name';
   private orcidDetailsUrl = 'https://bridge.metadatacenter.orgx/ext-auth/orcid';
 
-  constructor(
-    private http: HttpClient,
-    private messageHandlerService: MessageHandlerService,
-  ) {}
+  constructor(private http: HttpClient) {}
 
   // setRorSearchUrl(rorSearchUrl: string): void {
-  setRorSearchUrl(): void {
+  setOrcidSearchUrl(): void {
     // this.terminologyIntegratedSearchUrl = terminologyIntegratedSearchUrl;
     this.orcidSearchUrl = 'https://bridge.metadatacenter.orgx/ext-auth/orcid/search-by-name';
   }
@@ -31,7 +26,7 @@ export class OrcidFieldDataService {
     const randomDelay = Math.floor(Math.random() * 500);
     return timer(randomDelay).pipe(
       switchMap(() =>
-        this.http.get<RorSearchResponse>(this.orcidSearchUrl, { params }).pipe(
+        this.http.get<OrcidSearchResponse>(this.orcidSearchUrl, { params }).pipe(
           map((response) => {
             console.log('Orcid response', response);
             const results: OrcidSearchResponseItem[] = Object.keys(response.results).map((key) => ({
@@ -50,7 +45,11 @@ export class OrcidFieldDataService {
   }
 
   getDetails(id: string): Observable<any> {
+    console.log('id', id);
+    console.log('id', id);
     const encodedId = encodeURIComponent(id);
-    return this.http.get<any>(`${this.orcidDetailsUrl}/${encodedId}`, {});
+    const result = this.http.get<any>(`${this.orcidDetailsUrl}/${encodedId}`, {});
+    console.log('Details Orcid', result.subscribe());
+    return result;
   }
 }
