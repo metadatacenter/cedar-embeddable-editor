d67105020d6bb516ee1d1ea6f35a5a0ce48ff058
Mete Ugur Akdogan <muakdogan@gmail.com>
2025-04-10 16:49:22 -0700

    updates to remove extra js files

--- src/app/modules/shared/models/info/number-info.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NumberInfo = void 0;
-var NumberInfo = /** @class */ (function () {
-    function NumberInfo() {
-    }
-    return NumberInfo;
-}());
-exports.NumberInfo = NumberInfo;
